57a51eb0d7e7ef0c2f19ff54342ee75363be72d2
Anton Hansson <hansson@google.com>
2018-05-01 17:51:20 +0100

    apps/TV: Update path to android.jar.
    This file is being moved. Update the path here so that the old file
    can be deleted.
    
    Bug: 77525052
    Test: No
    Change-Id: Ieff6f28c2093b180d02f7e098d39b0985b9a4bb5

--- jni/gen_jni.sh
+++ jni/gen_jni.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-javah -jni -classpath ../../bin/classes:../../../../../../prebuilts/sdk/current/android.jar -o tunertvinput_jni.h com.android.tv.tuner.TunerHal
+javah -jni -classpath ../../bin/classes:../../../../../../prebuilts/sdk/current/public/android.jar -o tunertvinput_jni.h com.android.tv.tuner.TunerHal
