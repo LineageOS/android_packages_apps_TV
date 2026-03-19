7b234d146294d06327f75ec26313ad5b7102efe0
Shubang Lu <shubang@google.com>
2026-03-19 16:21:22 -0700

    Fix Intent redirection issue in SetupPassthroughActivity
    Fix: 332883816
    Test: mmm
    Flag: EXEMPT BUGFIX
    Cherrypick-From: https://googleplex-android-review.googlesource.com/q/commit:419fb3da8c29f17bb2d8681d48853fe481baf0ba
    Merged-In: I2a4cfa24b6c773cfa303e23de75b209bd6e7d83e
    Change-Id: I2a4cfa24b6c773cfa303e23de75b209bd6e7d83e

--- src/com/android/tv/SetupPassthroughActivity.java
+++ src/com/android/tv/SetupPassthroughActivity.java
@@ -21,6 +21,7 @@ import android.content.ActivityNotFoundException;
 import android.content.ComponentName;
 import android.content.Intent;
 import android.media.tv.TvInputInfo;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
@@ -116,6 +117,17 @@ public class SetupPassthroughActivity extends Activity {
             InputSetupActionUtils.removeSetupIntent(extras);
             setupIntent.putExtras(extras);
             try {
+                if (Build.VERSION.SDK_INT >= 34) { // Android 14(U)
+                    String callingPackage = getLaunchedFromPackage();
+                    if (callingPackage == null || !callingPackage.equals(
+                            CommonConstants.BASE_PACKAGE)) {
+                        Log.w(TAG, "Calling package " + callingPackage
+                                + " is not trusted. Not forwarding intent.");
+                        finish();
+                        return;
+                    }
+                }
+
                 ComponentName callingActivity = getCallingActivity();
                 if (callingActivity == null
                         || !callingActivity.getPackageName().equals(CommonConstants.BASE_PACKAGE)) {
